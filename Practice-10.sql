a5960e5987266bbf65117d3b6b1f02a8689e8ffc
Youngho Lee <bbchoi6@hufs.ac.kr>
2023-03-25 13:45:21 +0900

    계층형 조회(Oracle)

--- Practice-10.sql
+++ Practice-10.sql
@@ -1,37 +1,3 @@
--- 조인을 통한 합집합 구현
--- UNION : 두 개 이상의 테이블을 하나로 만드는 연산이다. 중복을 제거하기 때문에 정렬이 발생한다.
--- 주의 사항은 두 개 이상의 테이블의 컬럼 수, 컬럼의 데이터 타입이 무조건 일치해야 한다.
-
--- 아래 코드는 중복된 내용이 없기 때문에 20개 출력
-SELECT U.USERID, U.HEIGHT
- FROM USERTBL U
-UNION
-SELECT B.USERID, B.PRICE
- FROM BUYTBL B;
--- 아래 코드는 중복된 내용이 있지만 제거를 하고 난 뒤 10개만 출력되었다. 
-SELECT U.USERID
- FROM USERTBL U
-UNION
-SELECT B.USERID
- FROM BUYTBL B;
- 
--- UNION ALL은 중복된 레코드까지 다 합집으로 만든다. 정렬과정이 발생하지 않는다.
-SELECT U.USERID
- FROM USERTBL U
-UNION ALL
-SELECT B.USERID
- FROM BUYTBL B;
- 
- 
--- 차집합은 MINUS 연산을 한다. 먼저 쓴 SELECT문에는 있고, 뒤에 쓰는 SELECT 문에는 없는 집합을 의미한다.
--- MS-SQL에서 똑같은 기능을 하는 것은 EXCEPT가 있다. MYSQL에서는 지원하지 않는다.
--- 아래 쿼리는 구매한 USERID가 5개 밖에 없으므로 결과는 5개의 레코드가 나온다.
-SELECT U.USERID
- FROM USERTBL U
-MINUS
-SELECT B.USERID
- FROM BUYTBL B;
- 
 -- 계층형 조회(CONNECT BY) : 오라클에서만 지원을 해준다. 자체조인(셀프조인) 방법을 사용한다.
 DROP TABLE EMP;
 CREATE TABLE EMP(
@@ -78,12 +44,8 @@ CONNECT BY PRIOR EMPNO = MGR
 ORDER BY MGR, EMPNO;
 
 
-
--- 계층형 조회 결과를 명확히 보기 위해서 LPAD함수를 사용
+-- 계층형 조회 결과를 명확히 보기 위해서 LPAD함수를 사용 (padding)
 SELECT LEVEL, LPAD(' ', 4 * (LEVEL - 1) ) || EMPNO, MGR, CONNECT_BY_ISLEAF
  FROM EMP
 START WITH MGR IS NULL
 CONNECT BY PRIOR EMPNO = MGR;
-
-
--- CONNECT BY 키워드는 내용이 무엇인지는 다시한번 복기
