217f64ee3b1c9e85e130129640365c94f508093f
Youngho Lee <bbchoi6@hufs.ac.kr>
2023-03-25 13:31:17 +0900

    내장 함수

--- Practice-6.sql
+++ Practice-6.sql
@@ -23,6 +23,7 @@ SELECT '아름다운 ' || '대한민국'
 SELECT LOWER('ABCDeee'), UPPER('ABCDeee')
  FROM DUAL;
  
+
 -- 내장형 함수(날짜형 함수)
 -- SYSDATE : 지금 날짜를 출력
 -- EXTRACT() : 년, 월, 일을 추출해서 출력
@@ -33,6 +34,7 @@ SELECT SYSDATE, EXTRACT(YEAR FROM SYSDATE), EXTRACT(MONTH FROM SYSDATE), EXTRACT
 SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD HH:MM:DD AM')
  FROM DUAL;
  
+
 -- 내장형 함수(숫자형 함수)
 -- ABS() : 절댓값을 구하는 함수
 -- SIGN() : 양수, 0, 음수인지를 확인하는 함수(1이면 양수, -1이면 음수)
@@ -43,57 +45,3 @@ SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD HH:MM:DD AM')
 -- TRUNC(실수, 자릿수) : 자릿수로 만드는데 절삭 처리를 하는 함수
 SELECT ABS(-1), SIGN(0), MOD(11, 4), CEIL(10.9), FLOOR(10.1), ROUND(15.255, 2), TRUNC(15.255, 1)
  FROM DUAL;
- 
- 
- -- DECODE()함수 : IF문으로 구현할 수 있다.
- -- DECODE()함수는 컬럼값이 비교값과 비교해서 그 결과가 참이면 3번째 인자값을 리턴, 거짓이면 4번째 인자값 리턴
-SELECT DECODE(EMPNO, 1000, '참', '거짓')  
- FROM EMP; 
- 
-SELECT *
- FROM EMP;
- 
-SELECT DECODE(MGR, NULL, 'NULL값이다.', 'NULL값이 아니다.')
- FROM EMP;  
- 
--- 성별이 남자이면 M, 여자이면 F를 출력하는 DECODE문을 작성하시오.
--- DECODE(성별, '남자', 'M', 'F');
-
--- CASE문 : 프로그래밍 언어처럼 조건문을 사용할 수 있다.
-SELECT CASE
-            WHEN EMPNO = 1000 THEN 'A'
-            WHEN EMPNO = 1001 THEN 'B'
-            ELSE 'C'
-          END
- FROM EMP;
- 
- -- DEPTNO가 10번이면 '인사팀', 20번이면 '총무팀', 30번이면 '전산팀', 그 외 나머지는 '부서없음' 표식하시오. 단, CASE문을 이용하시오.
- -- CASE WHEN THEN ELSE END 문법을 알 수 있도록 하자.
- SELECT CASE
-            WHEN DEPTNO = 10 THEN '인사팀'
-            WHEN DEPTNO = 20 THEN '총무팀'
-            WHEN DEPTNO = 30 THEN '전산팀'
-            ELSE '부서없음'
-          END
- FROM EMP;
- 
- 
-
-
-
-SELECT * 
- FROM ( SELECT ROWNUM list, ENAME
-             FROM EMP)
-WHERE list <= 5; 
-
-
-SELECT * FROM   -- MAIN QUERY
-(SELECT * FROM EMP) LIST;  -- SUB QUERY(Inline view)
-
--- 웹 게시판에서 많이 사용되는 SELECT 구문이다.
-SELECT *
- FROM ( SELECT ROWNUM LIST, ENAME
-             FROM EMP)
-WHERE LIST BETWEEN 5  AND 10;   -- 특정 행만 조회한다.
-
-select rowid,  winetypename from winetype;
