82e043e5d57983afce5ec1a65e2a2bb124a90895
Youngho Lee <bbchoi6@hufs.ac.kr>
2023-03-25 13:28:02 +0900

    GROUP BY 그룹화

--- Practice-5.sql
+++ Practice-5.sql
@@ -23,44 +23,13 @@ INSERT INTO EMP VALUES(1010, 'TEST11', 20, '1002', 'CLERK', 1100);
 
 COMMIT;
 
-SELECT *
- FROM EMP;
-
--- NULL값의 특징 : 모르는 값, 값의 부재, 숫자 혹은 날짜를 더하면 NULL이 된다. 다른 값과 비교할 때 '알수 없음' 반환
--- IS NULL
-SELECT * 
- FROM EMP
-WHERE MGR IS NULL;
-
--- IS NOT NULL
-SELECT * 
- FROM EMP
-WHERE MGR IS NOT NULL;
-
--- NVL() 함수
-SELECT NVL(MGR, 0)
- FROM EMP;
- 
--- NVL2() 함수 - NVL()와 DECODE()함수를 하나로 만든 것
-SELECT NVL2(MGR, 1, 0)
- FROM EMP;
- 
--- NULLIF() 함수
-SELECT NULLIF(10, 10)
- FROM DUAL;
- 
--- COALESCE() 함수 : NULL이 아닌 최초의 인자값을 반환한다.\
-SELECT COALESCE(NULL, NULL, NULL, -100)
- FROM DUAL;
-
-
--- GROUP BY구문-집계함수와 같이 사용함
+-- GROUP BY구문 - 집계함수와 같이 사용
 SELECT DEPTNO AS "부서번호", SUM(SAL) AS "연봉합계"     -- 3
  FROM EMP   -- 1
 GROUP BY DEPTNO    -- 2 
 ORDER BY DEPTNO;    -- 4
 
--- 그룹핑 된 내용에 조건식을 주기 위해서는 HAVING구문을 사용한다.HAVING 구문은 항상 GROUP BY 와 함께 사용된다.
+-- 그룹핑 된 내용에 조건식을 주기 위해서는 HAVING구문을 사용. HAVING 구문은 항상 GROUP BY 와 함께 사용된다.
 SELECT DEPTNO AS "부서번호", SUM(SAL) AS "연봉합계"     -- 4
  FROM EMP   -- 1
 GROUP BY DEPTNO    -- 2
@@ -70,6 +39,7 @@ ORDER BY DEPTNO;    -- 5
 -- COUNT()함수 : 인자값으로 *(전체), 컬럼명(NULL)값을 제외한 컬럼수 반환
 SELECT COUNT(*)
  FROM EMP;
+
 -- MGR 컬럼은 2개의 NULL값이 존재하므로 9개의 레코드가 출력된다. 
 SELECT COUNT(MGR)
  FROM EMP;
@@ -81,6 +51,7 @@ SELECT DEPTNO AS "부서", MGR AS "관리자", FLOOR(AVG(SAL)) AS "평균급여"
 GROUP BY DEPTNO, MGR
 ORDER BY DEPTNO;
 
+
 -- 직업별(JOB), 급여합계 중에서 1000이상인 직업 추출
 SELECT JOB AS "직업", SUM(SAL) AS "급여합계"
  FROM EMP
@@ -88,6 +59,7 @@ SELECT JOB AS "직업", SUM(SAL) AS "급여합계"
 GROUP BY JOB
 HAVING SUM(SAL) >= 5000
 ORDER BY JOB;
+
 
 -- 사원번호가 1000 ~ 1003번까지의 부서별 급여 합계
 SELECT DEPTNO AS "부서코드", SUM(SAL) AS "급여합계"
